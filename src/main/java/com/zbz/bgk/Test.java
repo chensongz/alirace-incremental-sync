1f69cc539818b65613a1562ff5333fd39b91dab9
bgk <bgk083@163.com>
2017-06-16 22:57:56 +0800

    just for test

--- src/main/java/com/zbz/bgk/Test.java
+++ src/main/java/com/zbz/bgk/Test.java
@@ -1,8 +1,13 @@
 package com.zbz.bgk;
 
+import com.carrotsearch.hppc.LongObjectHashMap;
 import com.zbz.Binlog;
 import com.zbz.BinlogFactory;
 import com.zbz.BinlogReducer;
+import gnu.trove.map.hash.TLongLongHashMap;
+import gnu.trove.map.hash.TLongObjectHashMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectArrayMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 
 import java.util.*;
 
@@ -56,11 +61,11 @@ public class Test {
 //        String line1 = "|mysql-bin.000017659728416|1496746079000|middleware3|student|U|id:1:1|998|999|first_name:2:0|高|张|";
 //        String line2 = "|mysql-bin.000017659728416|1496746079000|middleware3|student|U|id:1:1|999|1999996|first_name:2:0|张|高|score:1:0|90|80";
         String line1 = "|mysql-bin.000017659728416|1496746079000|middleware3|student|I|id:1:1|NULL|999|first_name:2:0|高|张|";
-        String line2 = "|mysql-bin.000017659728416|1496746079000|middleware3|student|D|id:1:1|999|NULL|first_name:2:0|张|高|score:1:0|90|80";
+//        String line2 = "|mysql-bin.000017659728416|1496746079000|middleware3|student|D|id:1:1|999|NULL|first_name:2:0|张|高|score:1:0|90|80";
 
 //        String line1 = "|mysql-bin.000017659728416|1496746079000|middleware3|student|I|id:1:1|NULL|1999996|first_name:2:0|张|高|score:1:0|90|80";
 //        String line2 = "|mysql-bin.000017659728416|1496746079000|middleware3|student|U|id:1:1|1999996|9997|first_name:2:0|高|张|";
-//        Binlog binlog1 = BinlogFactory.createBinlog(line1);
+        Binlog binlog1 = BinlogFactory.createBinlog(line1);
 //        Binlog binlog2 = BinlogFactory.createBinlog(line2);
 //        System.out.println(binlog1);
 //        System.out.println("binlog1 primary key:" + binlog1.getPrimaryKey());
@@ -78,12 +83,34 @@ public class Test {
 //        System.out.println("binlog primary vaule:" + binlog.getPrimaryValue());
 //        System.out.println("binlog primary old vaule:" + binlog.getPrimaryOldValue());
 
-        BinlogReducer binlogReducer = new BinlogReducer("middleware3", "student");
-        binlogReducer.reduce(line1);
-        binlogReducer.reduce(line2);
+//        BinlogReducer binlogReducer = new BinlogReducer("middleware3", "student");
+//        binlogReducer.reduce(line1);
+//        binlogReducer.reduce(line2);
+//
+//        for (Binlog binlog : binlogReducer.getBinlogHashMap().values()) {
+//            System.out.println(binlog);
+//        }
+//        TLongObjectHashMap<Binlog> tLongObjectHashMap = new TLongObjectHashMap<>();
+//        LongObjectHashMap<Binlog> longObjectHashMap = new LongObjectHashMap<>();
+//        Long2ObjectOpenHashMap<Binlog> long2ObjectArrayMap = new Long2ObjectOpenHashMap<>();
+//        TLongLongHashMap tLongLongHashMap = new TLongLongHashMap();
+//        long t1 = System.currentTimeMillis();
+//        Map<Long, Binlog> hashMap = new TreeMap<>();
+//        for (long i = 0; i < 10000000; i++) {
+//            long2ObjectArrayMap.put(i, binlog1);
+//        }
+//        long t2 = System.currentTimeMillis();
+//        System.out.println("use time:" + (t2-t1));
+//        for (long i = 0; i < 10000000; i++) {
+//            long2ObjectArrayMap.get(i);
+//        }
+////        hashMap = null;
+//        long t3 = System.currentTimeMillis();
+//        System.out.println(tLongObjectHashMap.size());
+//        System.out.println("use time:" + (t3-t2));
 
-        for (Binlog binlog : binlogReducer.getBinlogHashMap().values()) {
-            System.out.println(binlog);
-        }
+
+
+
     }
 }
